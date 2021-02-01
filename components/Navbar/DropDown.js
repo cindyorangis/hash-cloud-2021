7db90cf4a2f4a23ffa510daf9b8e9d48bc6a4f1c
Cindy Le <cindyledev@gmail.com>
2021-01-31 22:36:02 -0500

    add profile view

--- components/Navbar/DropDown.js
+++ components/Navbar/DropDown.js
@@ -9,12 +9,12 @@ export default function DropDown() {
       </div>
       <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
         <Link href="/profile">
-          <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
+          <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
         </Link>
         <Link href="/settings">
-          <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
+          <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
         </Link>
-        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
+        <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
       </div>
     </div>
   )
--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,22 @@
+import { useState, useEffect } from 'react'
+import { Auth } from 'aws-amplify'
+
 export default function Profile() {
+  useEffect(() => {
+    checkUser()
+    async function checkUser() {
+      const user = await Auth.currentAuthenticatedUser()
+      console.log({ user })
+    }
+  }, [])
+
   return (
-    <h1>Profile Page</h1>
+    <div>
+      <button 
+        onClick={() => Auth.federatedSignIn({ provider: "Google" })}
+      >
+        Sign in with Google
+      </button>
+    </div>
   )
 }
