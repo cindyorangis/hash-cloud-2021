c63e1d9c2bd02c8b502d176308da9d3a39ec322c
Cindy Le <cindyledev@gmail.com>
2021-02-13 23:44:10 -0500

    add protected route for settings

--- pages/settings.js
+++ pages/settings.js
@@ -1,3 +1,31 @@
+import { Auth } from 'aws-amplify';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import '../configureAmplify';
+
 export default function Settings() {
-  return <h1>Settings Page</h1>;
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  async function checkUser() {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      setUser(user);
+    } catch (err) {
+      setUser(null);
+      router.push('/profile');
+    }
+  }
+
+  if (!user) return null;
+
+  return (
+    <div>
+      <p>Settings Page</p>
+    </div>
+  );
 }
