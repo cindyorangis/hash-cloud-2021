2c71ac2116b5a475b4fa11ffc3cfdbf5d2561c09
Cindy Le <cindyledev@gmail.com>
2021-02-13 21:20:38 -0500

    add jest for testing

---
+++ sum.js
@@ -0,0 +1,7 @@
+// Please delete this and sum.test.js
+
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
---
+++ test/sum.test.js
@@ -0,0 +1,7 @@
+// Please delete this and sum.js
+
+const sum = require('../sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
