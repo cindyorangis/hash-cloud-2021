788162fac7bef62005a328a5911af8fab4a24eb7
Cindy Le <cindyledev@gmail.com>
2021-02-14 18:54:48 -0500

    add SSR support for api/hello

--- configureAmplify.js
+++ configureAmplify.js
@@ -1,3 +1,6 @@
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
+Amplify.configure({
+  ...config,
+  ssr: true,
+});
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+import { withSSRContext } from 'aws-amplify';
+import '../../configureAmplify';
+
+export default async (req, res) => {
+  const { Auth } = withSSRContext({ req });
+  const user = await Auth.currentAuthenticatedUser();
+
   res.statusCode = 200;
-  res.json({ name: 'John Doe' });
+  res.json({ name: user });
 };
