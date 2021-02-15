d8032677df08ea8f96f289cf99370134012102d9
Cindy Le <cindyledev@gmail.com>
2021-02-14 23:19:50 -0500

    add graphql api

--- aws-exports.js
+++ aws-exports.js
@@ -20,7 +20,11 @@ const awsmobile = {
         "redirectSignOut": "htt://localhost:3000/",
         "responseType": "code"
     },
-    "federationTarget": "COGNITO_USER_POOLS"
+    "federationTarget": "COGNITO_USER_POOLS",
+    "aws_appsync_graphqlEndpoint": "https://4t5fnojqsbhdziubjssxrgbo2q.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-6ugxfnj76fcy5ksvtyd72eraku"
 };
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
+  plugins: [require('@tailwindcss/typography')],
 };
