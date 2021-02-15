6e811bc9684f46136b50e1fde0daffa69fbb83ae
Cindy Le <cindyledev@gmail.com>
2021-02-14 23:04:09 -0500

    add dependencies and fix tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      spacing: {
-        540: '540px',
-      },
-      boxShadow: {
-        form: '0 15px 35px 0 rgba(60,66,87,.08) 0 5px 15px 0 rgba(0,0,0,.012)',
-        inputfocus: '0 0 0 2px #F472B6',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 };
