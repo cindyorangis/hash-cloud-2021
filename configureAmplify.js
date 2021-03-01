33a1e7a3b91607b8a992670c4d8ea78ea0a84ce8
Cindy Le <cindyledev@gmail.com>
2021-03-01 14:33:18 -0500

    debugging authentication

--- configureAmplify.js
+++ configureAmplify.js
@@ -1,8 +1,15 @@
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import awsmobile from './aws-exports';
+
+const DEV_URL = 'http://localhost:3000'
+
+if (process.env.NODE_ENV === 'development') {
+  awsmobile.oauth.redirectSignIn = DEV_URL;
+  awsmobile.oauth.redirectSignOut = DEV_URL;
+}
 
 const updatedAwsConfig = {
-  ...config,
+  ...awsmobile,
   ssr: true,
 };
 
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    DEV_URL: process.env.DEV_URL,
+  },
+};
