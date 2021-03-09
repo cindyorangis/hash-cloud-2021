7b047af6e58e47e25477910d7fe320575c101499
Cindy Le <cindyledev@gmail.com>
2021-03-09 16:00:24 -0500

    update dependencies and run prettier

--- configureAmplify.js
+++ configureAmplify.js
@@ -1,7 +1,7 @@
 import Amplify from 'aws-amplify';
 import awsmobile from './aws-exports';
 
-const DEV_URL = 'http://localhost:3000'
+const DEV_URL = 'http://localhost:3000';
 
 if (process.env.NODE_ENV === 'development') {
   awsmobile.oauth.redirectSignIn = DEV_URL;
